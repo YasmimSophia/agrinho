f07e2611a2118b2ffd8436bef0db05a3cf0262bd
YasmimSophia <rodrigues.santos.yasmim@escola.pr.gov.br>
2025-05-31 02:28:08 +0000

    arrumando erros no codigo

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-function atualizartempo(){
-
-
-var display = document.querySelector (".display");
-var relogio = new Date();
-var horario = corrigirHorario(relogio.getHours()) =":"
- + corrigirHorario(relogio.getMinutes()) +":" + corrigirHorario(relogio.getSeconds());
-display.textContent = horario;
+function atualizarTempo() {
+    const display = document.querySelector(".display");
+    const relogio = new Date();
+    const horario = `${corrigirHorario(relogio.getHours())}:${corrigirHorario(relogio.getMinutes())}:${corrigirHorario(relogio.getSeconds())}`;
+    display.textContent = horario;
 }
 
-function corrigirHorario(numero){
-if (numero< 10){
- numero = "0" + numero;
-
-}
-return numero;
+function corrigirHorario(numero) {
+    return numero < 10 ? `0${numero}` : numero;
 }
 
 atualizarTempo();
-setInterval(atualizarTempo, 1000);   
-   
-console.log(horario);
+setInterval(atualizarTempo, 1000);
 
